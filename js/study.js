4af8d98621d0d0eb0fdfd3754161dae9401e8950
shin-eekk <shineek0525@gmail.com>
2025-02-05 10:07:22 +0900

    첫번째

---
+++ js/study.js
@@ -0,0 +1,28 @@
+const mynavi =  [
+    {
+        d1text : "계획짜기",
+        d1herf : "reservation.html"
+    },  
+    {
+        d1text : "트립톡",
+        d1herf : "triptalk.html"
+    },
+    {
+        d1text : "이벤트",
+        d1herf : "promotion.html"
+    },
+    {
+        d1text : "마이페이지",
+        d1herf : "mypage.html"
+    },
+    {
+        d1text : "고객센터",
+        d1herf : "mypage.html"
+    }
+
+]
+
+document.querySelector("#ek_gnb").innerHTML = "출력  =는 대입하다(버리고 받는다)"
+document.querySelector("#ek_gnb").innerHTML += "+=는 뒤에 이어서 들어가기"
+document.querySelector("#ek_gnb").innerHTML = ""
+
